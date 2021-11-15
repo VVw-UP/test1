ba0130e71d107a3fa2d4dbc879d75f76dd883fdb
weiwei <1257101304@qq.com>
2021-11-15 21:36:31 +0800

    蒋小威

--- test1/src/com/xwnb/Person.java
+++ test1/src/com/xwnb/Person.java
@@ -5,5 +5,37 @@ public class Person {
     private String name;
     private String email;
     private String classroom;
-//gagagagagag
+//gagagagaga
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getClassroom() {
+        return classroom;
+    }
+
+    public void setClassroom(String classroom) {
+        this.classroom = classroom;
+    }
 }
